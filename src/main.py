c1907205c8a4e298006dd75833df59a6cbff9ce8
sergio <sergio-r1@outlook.com>
2025-09-11 17:05:12 -0300

    refactor: restructure bot creation process with `build_bot` function and integrate `MessageHandler`

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from pyrogram import Client
 
 import settings
+from src.handlers import messages
 
 
-def main():
-    app = Client(
+def build_bot() -> Client:
+    bot_app = Client(
         name=settings.BOT_NAME,
         api_id=settings.TELEGRAM_API_ID,
         api_hash=settings.TELEGRAM_API_HASH,
         bot_token=settings.TELEGRAM_BOT_TOKEN
     )
-    app.run()
+    messages.MessageHandler(bot_app)
+    return bot_app
+
+
+def main():
+    bot = build_bot()
+    bot.run()
 
 
 if __name__ == "__main__":
